2e43a6678f261dbe6af24bba98cf673259963dfa
Ashwin Joseph Kurian <30999857+ashwinjoseph95@users.noreply.github.com>
2018-06-22 01:45:25 +0400

    Add files via upload

---
+++ videodetect.py
@@ -0,0 +1,34 @@
+from imageai.Detection import VideoObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = VideoObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+
+video_path = detector.detectObjectsFromVideo(input_file_path=os.path.join(execution_path, "walking.avi"),
+                                output_file_path=os.path.join(execution_path, "detected")
+                                , frames_per_second=20, log_progress=True)
+print(video_path)
+'''
+#Meant for custom object detections if not all required.
+#The video object detection model (RetinaNet) supported by ImageAI can detect 80 different types of objects. They include:
+      person,   bicycle,   car,   motorcycle,   airplane,
+          bus,   train,   truck,   boat,   traffic light,   fire hydrant,   stop_sign,
+          parking meter,   bench,   bird,   cat,   dog,   horse,   sheep,   cow,   elephant,   bear,   zebra,
+          giraffe,   backpack,   umbrella,   handbag,   tie,   suitcase,   frisbee,   skis,   snowboard,
+          sports ball,   kite,   baseball bat,   baseball glove,   skateboard,   surfboard,   tennis racket,
+          bottle,   wine glass,   cup,   fork,   knife,   spoon,   bowl,   banana,   apple,   sandwich,   orange,
+          broccoli,   carrot,   hot dog,   pizza,   donot,   cake,   chair,   couch,   potted plant,   bed,
+          dining table,   toilet,   tv,   laptop,   mouse,   remote,   keyboard,   cell phone,   microwave,
+          oven,   toaster,   sink,   refrigerator,   book,   clock,   vase,   scissors,   teddy bear,   hair dryer,
+          toothbrush.
+custom_objects = detector.CustomObjects(person=True, bicycle=True, motorcycle=True)
+
+video_path = detector.detectCustomObjectsFromVideo(custom_objects=custom_objects, input_file_path=os.path.join(execution_path, "traffic.mp4"),
+                                output_file_path=os.path.join(execution_path, "traffic_custom_detected")
+                                , frames_per_second=20, log_progress=True)
+print(video_path)
+'''
